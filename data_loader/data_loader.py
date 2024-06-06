7d9e983ddf6b849e1063d3617bc433b26241a8ce
drawcodeboy <20rnjsekdns@naver.com>
2024-06-06 16:28:11 +0900

    Add: Model & DataLoader

---
+++ data_loader/data_loader.py
@@ -0,0 +1,35 @@
+import torch
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+from torchvision import transforms
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+transform = transforms.Compose([
+    transforms.ToTensor(),
+])
+
+class CustomDataset(Dataset):
+    def __init__(self, df=pd.DataFrame, transform=None):
+        super().__init__()
+        
+        self.df = df
+        self.transform = transform
+    
+    def __len__(self):
+        return len(self.df)
+        
+    def __getitem__(self, index):
+        data = self.df.iloc[index].to_numpy()
+        
+        label = data[0]
+        sample = data[1:].reshape(28, 28).astype(np.float32)
+        
+        # Normalize
+        sample /= 255.0
+        
+        sample = torch.tensor(sample, dtype=torch.float32)
+        label = torch.tensor(label, dtype=torch.int64) # int64(long) for CrossEntropy
+        
+        return sample, label
---
+++ models/conv_net.py
@@ -0,0 +1,41 @@
+import torch
+from torch import nn
+from torchsummary import summary
+
+class ConvBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=(3, 3), padding=1)
+        self.bn = nn.BatchNorm2d(out_channels)
+        self.acti = nn.GELU()
+    
+    def forward(self, x):
+        return self.acti(self.bn(self.conv(x)))
+
+class ConvNet(nn.Module):
+    def __init__(self, in_channels=1):
+        super().__init__()
+        
+        self.conv_blk1 = ConvBlock(in_channels, 32)
+        self.conv_blk2 = ConvBlock(32, 64)
+        
+        self.flatten = nn.Flatten()
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        
+        self.l1 = nn.Linear(3136, 100)
+        self.l2 = nn.Linear(100, 10)
+        self.acti = nn.GELU()
+        
+    def forward(self, x):
+        x = self.conv_blk1(x)
+        x = self.pool(x)
+        
+        x = self.conv_blk2(x)
+        x = self.pool(x)
+        
+        x = self.flatten(x)
+        
+        x = self.acti(self.l1(x))
+        x = self.l2(x)
+        
+        return x
